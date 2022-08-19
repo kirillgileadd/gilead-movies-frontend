6a8ff4e06de1c4b7cc86be05a9539e96669affc2
kirillgileadd <kirillgilead@gmail.com>
2022-08-20 00:47:27 +0300

    admin panel page

--- pages/menage/index.tsx
+++ pages/menage/index.tsx
@@ -1,10 +1,17 @@
 import React from 'react'
 
+import Layout from '@/components/layout/Layout'
+
+import Admin from '../../app/screens/admin/Admin'
 import { NextPageAuth } from '../../app/types/auth.types'
 
 
 const MenagePage: NextPageAuth = () => {
-	return <div>AdminPanel</div>
+	return (
+		<Layout>
+			<Admin />
+		</Layout>
+	)
 }
 
 MenagePage.isOnlyAdmin = true
